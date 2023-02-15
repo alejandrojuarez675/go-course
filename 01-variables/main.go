e11634741a3295ff52682a9403bca36a5e5a0258
Alejandro Juarez <ext_alejjuar@mercadolibre.com>
2023-02-15 17:20:30 -0300

    variables

--- 01-variables/main.go
+++ 01-variables/main.go
@@ -3,6 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	var name string
-	fmt.Println(name)
+	var name1 = "Ale1" // declare dynamic var and assign value
+
+	var name2 string // declare var
+	name2 = "Ale2"   // assign value
+
+	name3 := "Ale3" // declare dynamic var and assign value
+
+	fmt.Println(name1)
+	fmt.Println(name2)
+	fmt.Println(name3)
+
+	// initializing multiple variables at same line
+	var name, lastname = "ale", "juarez"
+	fmt.Println(name, lastname)
+
+	name4, lastname4 := "ale", "juarez"
+	fmt.Println(name4, lastname4)
+
+	// constants
+	const Course = "Go course"
+	fmt.Println(Course)
 }
