1e09d38fd25ded4599b9461e8f21112a88bb3c5a
Alejandro Juarez <ext_alejjuar@mercadolibre.com>
2023-02-16 12:06:34 -0300

    adding lambda functions

--- 03-functions/main.go
+++ 03-functions/main.go
@@ -11,6 +11,13 @@ func main() {
 
 	isTurnedOn := true
 	log("formatted log is %v", isTurnedOn)
+
+	// lambdas func
+	isBefore20230304 := func() bool {
+		now := time.Now()
+		return now.Before(time.Date(2023, time.March, 4, 0, 0, 0, 0, time.Local))
+	}
+	log("is before 4/3/23 %v", isBefore20230304())
 }
 
 func log(format string, values ...any) {
